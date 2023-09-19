9a0704f867cd4ea9d943fd3027d8297a384b0885
OnilBlanco26 <onilblanco26@gmail.com>
2023-09-19 09:03:23 -0400

    db env

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require("sequelize");
 const db = new Sequelize({
     dialect: 'postgres',
     host: 'localhost',
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     logging: false
 })
 
